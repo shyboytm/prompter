29fd8be4227c17b8494eb04b0809d77c26a475f8
Dennis Cortes <denniscortes@denniss-mbp.attlocal.net>
2017-08-02 12:52:13 -0500

    Adds more nouns and adjectives

--- src/data/Adjectives.js
+++ src/data/Adjectives.js
@@ -1,6 +1,9 @@
 var adjectives = [
-  'Big', 'Cool', 'Happy', 'Nonchalant', 'Pixelated', 'Nerdy', 'Punk', 'Maroon', 'Violet',
-  'Pastel', 'Red', 'Blue', 'Icey', 'Burnt', 'Minimal', 'Gradient',
+  'Giant', 'Cool', 'Happy', 'Nonchalant', 'Pixelated', 'Nerdy', 'Punk', 'Maroon', 'Violet',
+  'Pastel', 'Red', 'Blue', 'Icey', 'Burnt', 'Minimal', 'Gradient', 'Sad', 'Mad', 'Crying',
+  'Gifted', 'Upscale', 'Slow', 'Evil', 'Angelic', 'Sippery', 'Light', 'Dark', 'Golden', 'Copper',
+  'Cosmic', 'Medieval', 'Ghostly', 'Expensive', 'Careful', 'Brave', 'Adorable', 'Beautiful', 'Fat',
+  'Zombie', 'Broad', 'Abandoned', 'Agitated', 'Angry', 'Cartoon', 'Clean', 'Cold'
 ];
 
 export default adjectives;
--- src/data/Nouns.js
+++ src/data/Nouns.js
@@ -1,7 +1,13 @@
 var nouns = [
   'Dog', 'Cat', 'Bird', 'Man', 'Woman', 'Girl', 'Boy', 'Human', 'Flag', 'Video Game',
   'Wizard', 'Toast', 'Breakfast', 'Landscape', 'Nature', 'Ocean', 'Pond', 'Lake', 'Ninja',
-  'Emoji',
+  'Emoji', 'Naruto', 'Sasuke', 'Sakura', 'iPhone', 'MacBook', 'Laptop', 'Apple', 'Pen', 'Pencil',
+  'Marker', 'Painting', 'Crayon', 'School', 'Nerd', 'Middle Schooler', 'Bottle', 'Desk', 'Jake the Dog',
+  'Fin the Human', 'Keyboard', 'Instrument', 'Violin', 'Cello', 'Drums', 'Bass', 'Rapper',
+  'Singer', 'Celebrity', 'Kanye West', 'Lil Wayne', 'Adele', 'Ariana Grande', 'Migos', 'Katy Perry',
+  'Whippet', 'Italian Greyhound', 'Daschund', 'Pitbull', 'Golden Retriever', 'Mutt', 'Monkey',
+  'Albert Einstein', 'Train', 'Laptop', 'Cartoon', 'Anime', 'Character', 'Office Chair', 'Website',
+  'Magazine', 'Newspaper', 'Car', 'Ferrari', 'Lambo', 'Tattoo', 'Wallet', 'Lamp', 'Pokémon'
 ];
 
 export default nouns;
