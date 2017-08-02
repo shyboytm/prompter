0791c37d3c8852f47914d54e579567ecd20f0a63
Dennis Cortes <denniscortes@denniss-mbp.attlocal.net>
2017-08-02 11:29:35 -0500

    Separates prompts into adjectives and nouns, also adds font styling and basic styling to site as well

--- src/components/Adjectives.js
+++ src/components/Adjectives.js
@@ -13,11 +13,10 @@ class App extends Component {
     var prompt = adjective + " " + noun;
 
     return (
-      <div className="Home bg-black-05 ma0 pa6-l pa5-m pa4">
+      <div className="Home system pa5 bg-black-05">
         <div className="tc">
           <p className="black-60 lh-copy">Your prompt for today is...</p>
-          <h1 className="mt4 mb6-l mb5 f-headline-l f-subheadline-m f1 vampiro">{prompt}</h1>
-          <a href="/" className="mv4 mb5 dib center white bg-gold hover-bg-orange bg-animate link pa3 fw4 pointer br2 system">New Prompt Please!</a>
+          <h1 className="mt4 f-headline-ns f1 vampiro">{prompt}</h1>
         </div>
       </div>
     );
